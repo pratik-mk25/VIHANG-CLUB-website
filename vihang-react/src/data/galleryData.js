7105d84aa65f41ba3755f0ddf2cedefd88088a97
pratik-mk25 <pkhadse253@gmail.com>
2026-06-15 19:33:36 +0530

    fix: gallery polaroid text overlap and category color

--- vihang-react/src/data/galleryData.js
+++ vihang-react/src/data/galleryData.js
@@ -3,84 +3,404 @@
 // Categories: 'COMPETITION', 'WORKSHOP', 'TESTING', 'EVENT', 'PROJECT'
 
 export const galleryData = [
-  { title: "A VERY DIFFERENT FRAME", cat: "PROJECT", img: "/gallery img/w3.jpeg" },
-  { title: "SAY CHEESE", cat: "EVENT", img: "/gallery img/w12.jpeg" },
-  { title: "VISIT BY OTHER COLLEGES", cat: "EVENT", img: "/gallery img/w1 (2).jpeg" },
-  { title: "THE BEAUTY WITH TORODIAL PROPS", cat: "PROJECT", img: "/gallery img/v.jpeg" },
-  { title: "VISIT TO GATEWAY OF INDIA FOR FINDING IDEAS", cat: "EVENT", img: "/gallery img/mumbai1.jpg" },
-  { title: "THE MATERIAL WAS ON HOSTEL ROOF", cat: "PROJECT", img: "/gallery img/w19.jpeg" },
-  { title: "HRISHIKESH & SAURABH WITH FLOOD ANALYSIS", cat: "COMPETITION", img: "/gallery img/hrishksh avishkar.jpeg" },
-  { title: "HRK EXPLAINING, PRANAV NOTING AND KUNAL STRATERGIZING @MKD NANDURBAR", cat: "COMPETITION", img: "/gallery img/mkd.jpg" },
-  { title: "AARAMBH HAI PRACHAND", cat: "EVENT", img: "/gallery img/w5.jpeg" },
-  { title: "FINAL YEAR PROJECT EXPO", cat: "PROJECT", img: "/gallery img/w8.jpeg" },
-  { title: "WAITING FOR SELECTION RESULT", cat: "WORKSHOP", img: "/gallery img/26 15.jpeg" },
-  { title: "FPV RELYING ON RUBBER-BANDS!", cat: "PROJECT", img: "/gallery img/project 2.jpg" },
-  { title: "SECOND AT TECHKRITI IIT KANPUR", cat: "COMPETITION", img: "/gallery img/y.jpeg" },
-  { title: "WE GO AND WE WIN", cat: "COMPETITION", img: "/gallery img/AVISHKAR.jpg" },
-  { title: "ESP BOARD PEAK PHOTOGRAPHY!", cat: "PROJECT", img: "/gallery img/esp.jpg" },
-  { title: "ARPIT & ATHARVA WITH AEROVISION", cat: "COMPETITION", img: "/gallery img/arpit avishkar.jpeg" },
-  { title: "SAHIL EXPLAINING HOW NOT TO BUILD DRONE & ARPIT MEASURING HOW MUCH WIRE SHOULD BE UTILISED", cat: "TESTING", img: "/gallery img/26 25.jpeg" },
-  { title: "MANTHAN FIXING THE BROKEN FRAME", cat: "TESTING", img: "/gallery img/26 20.jpeg" },
-  { title: "AMOL SURYAWANSHI SIR WITH OTHER ALUMNI LISTENING ABOUT OUR PROJECTS", cat: "EVENT", img: "/gallery img/visit 2.jpg" },
-  { title: "DEMONSTRATING PROJECT TO OUR BELOVED ALUMNIS", cat: "EVENT", img: "/gallery img/26 6.jpeg" },
-  { title: "LOOKS CUTE \"LETS FLY!\"", cat: "PROJECT", img: "/gallery img/26 7.jpeg" },
-  { title: "ARPIT BUILDING HIS VERY FAMOUS RC CAR(INSANELY WIRED)", cat: "TESTING", img: "/gallery img/26 24.jpeg" },
-  { title: "SOON ON THE BUDDHA CIRCUIT", cat: "PROJECT", img: "/gallery img/rc car.jpg" },
-  { title: "GIMBAL IT IS", cat: "PROJECT", img: "/gallery img/r.jpeg" },
-  { title: "NICE POSE FOR A PIC BUILDING GIMBAL", cat: "TESTING", img: "/gallery img/w1.jpeg" },
-  { title: "FEW MORE WITH DIFFERENT ANGLE", cat: "EVENT", img: "/gallery img/w25.jpeg" },
-  { title: "TEAM BEHIND TOROIDAL PROPS", cat: "PROJECT", img: "/gallery img/w10.jpeg" },
-  { title: "NOW CALCULATING THRUST BY TOROIDAL", cat: "PROJECT", img: "/gallery img/q.jpeg" },
-  { title: "CALCULATIONS FOR TOROIDAL DESIGN", cat: "PROJECT", img: "/gallery img/p.jpeg" },
-  { title: "SANCHIT GIVING HIS INTRODUCTION", cat: "WORKSHOP", img: "/gallery img/26 16.jpeg" },
-  { title: "THE TEAM BEFORE SNAP", cat: "EVENT", img: "/gallery img/26 10.jpeg" },
-  { title: "ANOTHER ATTEMPT FOR RC CAR", cat: "PROJECT", img: "/gallery img/w23.jpeg" },
-  { title: "FIRST RC CAR OF CLUB", cat: "PROJECT", img: "/gallery img/w7.jpeg" },
-  { title: "SAME PIC WITH MORE SMILES", cat: "EVENT", img: "/gallery img/expo2.jpg" },
-  { title: "THE TEAM WITH THE DICTATOR", cat: "PROJECT", img: "/gallery img/alumini25.jpg" },
-  { title: "PRANAV EXPLAINING PROJECT TO CEO OF SGBAU INCUBATION CENTRE", cat: "COMPETITION", img: "/gallery img/26 18.jpeg" },
-  { title: "ALIGNMENT AFTER MEASUREMENT", cat: "PROJECT", img: "/gallery img/w18.jpeg" },
-  { title: "PRANAV EXPRESSING HIS IDEAS", cat: "WORKSHOP", img: "/gallery img/w11.jpeg" },
-  { title: "2 CAPTAINS IN ONE FRAME DEEP AND TEJAS @GCOE JALGAON", cat: "COMPETITION", img: "/gallery img/z.jpeg" },
-  { title: "NIVRUTTI & PARTH WITH S.O.S", cat: "COMPETITON", img: "/gallery img/nd raut.jpeg" },
-  { title: "TECHNICAL HEAD MANTHAN KOLTE", cat: "PROJECT", img: "/gallery img/w21.jpeg" },
-  { title: "SIPNA 26 \"WHO IS THE GUY IN THE LEFT\"", cat: "COMPETITION", img: "/gallery img/26 5.jpeg" },
-  { title: "ANOTHER ANGLE OF SOLAR RC PLANE", cat: "PROJECT", img: "/gallery img/26 9.jpeg" },
-  { title: "BLACK WHALE WE CALL IT", cat: "PROJECT", img: "/gallery img/patent rc.jpg" },
-  { title: "ANOTHER VIEW OF BLACK WHALE", cat: "PROJECT", img: "/gallery img/rc 2.jpg" },
-  { title: "BLACK WHALE FABRICATION DONE", cat: "PROJECT", img: "/gallery img/w27.jpeg" },
-  { title: "TEJAS & SAHIL GOT THE FIRST UNIVERSITY COLOUR COAT OF CLUB", cat: "COMPETITION", img: "/gallery img/avishkar 2024.png" },
-  { title: "PHOTO BY A ROOKIE OBSTACLE AVOIDANCE", cat: "PROJECT", img: "/gallery img/obstacle avoid.jpg" },
-  { title: "FERRARI AND MERCEDES", cat: "COMPETITION", img: "/gallery img/w.jpeg" },
-  { title: "HACKING INTO GIS", cat: "WORKSHOP", img: "/gallery img/w16.jpeg" },
-  { title: "PARTH WITH A SOLDERING IRON", cat: "TESTING", img: "/gallery img/26 22.jpeg" },
-  { title: "THANK YOU FOR YOUR DONATION", cat: "EVENT", img: "/gallery img/a1.jpg" },
-  { title: "VERY BADLY CROPPED", cat: "EVENT", img: "/gallery img/rc plaane ath.jpeg" },
-  { title: "CHECKING THE CHARGING", cat: "PROJECT", img: "/gallery img/26 2.jpeg" },
-  { title: "CAN YOU GUESS WHAT THEY ARE TALKING", cat: "WORKSHOP", img: "/gallery img/w13.jpeg" },
-  { title: "GOODBYES TO THE FOUNDING BATCH", cat: "EVENT", img: "/gallery img/w15.jpeg" },
-  { title: "THE IV NIVRUTTI AND HRK(HRISIKESH) IN CLUB COLOURS", cat: "TESTING", img: "/gallery img/26 21.jpeg" },
-  { title: "EXPLANATION AT POINT AND ALSO THE STRATERGY WE CAME 1ST NOTES WILL HELP LATER", cat: "EVENT", img: "/gallery img/mkd2.jpg" },
-  { title: "CHECKING THE CHARGING ON THE STAND", cat: "PROJECT", img: "/gallery img/26 3.jpeg" },
-  { title: "LOOKS DOPE!", cat: "PROJECT", img: "/gallery img/a3025.jpg" },
-  { title: "THE MESS BEHIND THE GLORY", cat: "WORKSHOP", img: "/gallery img/w2.jpeg" },
-  { title: "TEAM OF 2024 @ALUMNI MEET 25", cat: "EVENT", img: "/gallery img/expo.jpg" },
-  { title: "THE TEAM AT ALUMNI MEET 25", cat: "EVENT", img: "/gallery img/a2.jpg" },
-  { title: "CORNERED AT ALUMNI MEET 26 BUT WE ARE CENTER OF ATTRACTION", cat: "EVENT", img: "/gallery img/26 1.jpeg" },
-  { title: "ANOTHER ANGLE OF BLACK WHALE", cat: "PROJECT", img: "/gallery img/rc merge.jpg" },
-  { title: "BLACK WHALE READY TO FLY", cat: "PROJECT", img: "/gallery img/w17.jpeg" },
-  { title: "THE BUILDING OF BLACK WHALE", cat: "WORKSHOP", img: "/gallery img/w14.jpeg" },
-  { title: "ANOTHER WIN TO CONTINUE LEGACY!", cat: "COMPETITION", img: "/gallery img/26 12.jpeg" },
-  { title: "ADITYA AND VAIBHAV (VAIBHAV HAS NOT EATEN SO EATING THE WIRE?)", cat: "TESTING", img: "/gallery img/26 23.jpeg" },
-  { title: "THE VISIT THAT CHANGED SEVERAL LIVES", cat: "EVENT", img: "/gallery img/visit.jpg" },
-  { title: "VTOL UDD!", cat: "PROJECT", img: "/gallery img/26 8.jpeg" },
-  { title: "OUR VISIT TO DRONE EXPO 25 MUMBAI", cat: "EVENT", img: "/gallery img/26 14.jpeg" },
-  { title: "OUR TEAM AT AIR STRIP", cat: "EVENT", img: "/gallery img/a4.jpg" },
-  { title: "LETS PLAY WITH LASER", cat: "PROJECT", img: "/gallery img/w26.jpeg" },
-  { title: "THE PROJECTS TILL 2025", cat: "PROJECT", img: "/gallery img/a2025.jpg" },
-  { title: "STILL CATCHING UP!", cat: "EVENT", img: "/gallery img/expo3.jpg" },
-  { title: "WANNA LEARN PCBs", cat: "WORKSHOP", img: "/gallery img/workhop1.jpg" },
-  { title: "LEGACY KO KISI KI NAZAR NA LAGE", cat: "COMPETITION", img: "/gallery img/trophy.jpg" },
-  { title: "FEW MORE PICS", cat: "EVENT", img: "/gallery img/w24.jpeg" },
-  { title: "THERE ARE 3 CAPTAINS IN ONE PIC", cat: "EVENT", img: "/gallery img/w6.jpeg" },
+  {
+    "title": "A VERY DIFFERENT FRAME",
+    "cat": "PROJECT",
+    "img": "/gallery img/w3.jpeg"
+  },
+  {
+    "title": "SAY CHEESE",
+    "cat": "EVENT",
+    "img": "/gallery img/w12.jpeg"
+  },
+  {
+    "title": "VISIT BY OTHER COLLEGES",
+    "cat": "EVENT",
+    "img": "/gallery img/w1 (2).jpeg"
+  },
+  {
+    "title": "THE BEAUTY WITH TORODIAL PROPS",
+    "cat": "PROJECT",
+    "img": "/gallery img/v.jpeg"
+  },
+  {
+    "title": "VISIT TO GATEWAY OF INDIA FOR FINDING IDEAS",
+    "cat": "EVENT",
+    "img": "/gallery img/mumbai1.jpg"
+  },
+  {
+    "title": "THE MATERIAL WAS ON HOSTEL ROOF",
+    "cat": "PROJECT",
+    "img": "/gallery img/w19.jpeg"
+  },
+  {
+    "title": "HRISHIKESH & SAURABH WITH FLOOD ANALYSIS",
+    "cat": "COMPETITION",
+    "img": "/gallery img/hrishksh avishkar.jpeg"
+  },
+  {
+    "title": "AARAMBH HAI PRACHAND",
+    "cat": "EVENT",
+    "img": "/gallery img/w5.jpeg"
+  },
+  {
+    "title": "FINAL YEAR PROJECT EXPO",
+    "cat": "PROJECT",
+    "img": "/gallery img/w8.jpeg"
+  },
+  {
+    "title": "WAITING FOR SELECTION RESULT",
+    "cat": "WORKSHOP",
+    "img": "/gallery img/26 15.jpeg"
+  },
+  {
+    "title": "FPV RELYING ON RUBBER-BANDS!",
+    "cat": "PROJECT",
+    "img": "/gallery img/project 2.jpg"
+  },
+  {
+    "title": "SECOND AT TECHKRITI IIT KANPUR",
+    "cat": "COMPETITION",
+    "img": "/gallery img/y.jpeg"
+  },
+  {
+    "title": "WE GO AND WE WIN",
+    "cat": "COMPETITION",
+    "img": "/gallery img/AVISHKAR.jpg"
+  },
+  {
+    "title": "ESP BOARD PEAK PHOTOGRAPHY!",
+    "cat": "PROJECT",
+    "img": "/gallery img/esp.jpg"
+  },
+  {
+    "title": "ARPIT & ATHARVA WITH AEROVISION",
+    "cat": "COMPETITION",
+    "img": "/gallery img/arpit avishkar.jpeg"
+  },
+  {
+    "title": "SAHIL EXPLAINING HOW NOT TO BUILD DRONE & ARPIT MEASURING HOW MUCH WIRE SHOULD BE UTILISED",
+    "cat": "TESTING",
+    "img": "/gallery img/26 25.jpeg"
+  },
+  {
+    "title": "MANTHAN FIXING THE BROKEN FRAME",
+    "cat": "TESTING",
+    "img": "/gallery img/26 20.jpeg"
+  },
+  {
+    "title": "AMOL SURYAWANSHI SIR WITH OTHER ALUMNI LISTENING ABOUT OUR PROJECTS",
+    "cat": "EVENT",
+    "img": "/gallery img/visit 2.jpg"
+  },
+  {
+    "title": "DEMONSTRATING PROJECT TO OUR BELOVED ALUMNIS",
+    "cat": "EVENT",
+    "img": "/gallery img/26 6.jpeg"
+  },
+  {
+    "title": "LOOKS CUTE \"LETS FLY!\"",
+    "cat": "PROJECT",
+    "img": "/gallery img/26 7.jpeg"
+  },
+  {
+    "title": "ARPIT BUILDING HIS VERY FAMOUS RC CAR(INSANELY WIRED)",
+    "cat": "TESTING",
+    "img": "/gallery img/26 24.jpeg"
+  },
+  {
+    "title": "SOON ON THE BUDDHA CIRCUIT",
+    "cat": "PROJECT",
+    "img": "/gallery img/rc car.jpg"
+  },
+  {
+    "title": "GIMBAL IT IS",
+    "cat": "PROJECT",
+    "img": "/gallery img/r.jpeg"
+  },
+  {
+    "title": "NICE POSE FOR A PIC BUILDING GIMBAL",
+    "cat": "TESTING",
+    "img": "/gallery img/w1.jpeg"
+  },
+  {
+    "title": "FEW MORE WITH DIFFERENT ANGLE",
+    "cat": "EVENT",
+    "img": "/gallery img/w25.jpeg"
+  },
+  {
+    "title": "TEAM BEHIND TOROIDAL PROPS",
+    "cat": "PROJECT",
+    "img": "/gallery img/w10.jpeg"
+  },
+  {
+    "title": "SANCHIT GIVING HIS INTRODUCTION",
+    "cat": "WORKSHOP",
+    "img": "/gallery img/26 16.jpeg"
+  },
+  {
+    "title": "THE TEAM BEFORE SNAP",
+    "cat": "EVENT",
+    "img": "/gallery img/26 10.jpeg"
+  },
+  {
+    "title": "HRK EXPLAINING, PRANAV NOTING AND KUNAL STRATERGIZING @MKD NANDURBAR",
+    "cat": "COMPETITION",
+    "img": "/gallery img/mkd.jpg"
+  },
+  {
+    "title": "EXPLANATION AT POINT AND ALSO THE STRATERGY WE CAME 1ST NOTES WILL HELP LATER",
+    "cat": "EVENT",
+    "img": "/gallery img/mkd2.jpg"
+  },
+  {
+    "title": "ANOTHER ATTEMPT FOR RC CAR",
+    "cat": "PROJECT",
+    "img": "/gallery img/w23.jpeg"
+  },
+  {
+    "title": "FIRST RC CAR OF CLUB",
+    "cat": "PROJECT",
+    "img": "/gallery img/w7.jpeg"
+  },
+  {
+    "title": "SAME PIC WITH MORE SMILES",
+    "cat": "EVENT",
+    "img": "/gallery img/expo2.jpg"
+  },
+  {
+    "title": "THE TEAM WITH THE DICTATOR",
+    "cat": "PROJECT",
+    "img": "/gallery img/alumini25.jpg"
+  },
+  {
+    "title": "PRANAV EXPLAINING PROJECT TO CEO OF SGBAU INCUBATION CENTRE",
+    "cat": "COMPETITION",
+    "img": "/gallery img/26 18.jpeg"
+  },
+  {
+    "title": "ALIGNMENT AFTER MEASUREMENT",
+    "cat": "PROJECT",
+    "img": "/gallery img/w18.jpeg"
+  },
+  {
+    "title": "PRANAV EXPRESSING HIS IDEAS",
+    "cat": "WORKSHOP",
+    "img": "/gallery img/w11.jpeg"
+  },
+  {
+    "title": "2 CAPTAINS IN ONE FRAME DEEP AND TEJAS @GCOE JALGAON",
+    "cat": "COMPETITION",
+    "img": "/gallery img/z.jpeg"
+  },
+  {
+    "title": "NIVRUTTI & PARTH WITH S.O.S",
+    "cat": "COMPETITON",
+    "img": "/gallery img/nd raut.jpeg"
+  },
+  {
+    "title": "TECHNICAL HEAD MANTHAN KOLTE",
+    "cat": "PROJECT",
+    "img": "/gallery img/w21.jpeg"
+  },
+  {
+    "title": "SIPNA 26 \"WHO IS THE GUY IN THE LEFT\"",
+    "cat": "COMPETITION",
+    "img": "/gallery img/26 5.jpeg"
+  },
+  {
+    "title": "ANOTHER ANGLE OF SOLAR RC PLANE",
+    "cat": "PROJECT",
+    "img": "/gallery img/26 9.jpeg"
+  },
+  {
+    "title": "BLACK WHALE WE CALL IT",
+    "cat": "PROJECT",
+    "img": "/gallery img/patent rc.jpg"
+  },
+  {
+    "title": "ANOTHER VIEW OF BLACK WHALE",
+    "cat": "PROJECT",
+    "img": "/gallery img/rc 2.jpg"
+  },
+  {
+    "title": "BLACK WHALE FABRICATION DONE",
+    "cat": "PROJECT",
+    "img": "/gallery img/w27.jpeg"
+  },
+  {
+    "title": "TEJAS & SAHIL GOT THE FIRST UNIVERSITY COLOUR COAT OF CLUB",
+    "cat": "COMPETITION",
+    "img": "/gallery img/avishkar 2024.png"
+  },
+  {
+    "title": "PHOTO BY A ROOKIE OBSTACLE AVOIDANCE",
+    "cat": "PROJECT",
+    "img": "/gallery img/obstacle avoid.jpg"
+  },
+  {
+    "title": "FERRARI AND MERCEDES",
+    "cat": "COMPETITION",
+    "img": "/gallery img/w.jpeg"
+  },
+  {
+    "title": "HACKING INTO GIS",
+    "cat": "WORKSHOP",
+    "img": "/gallery img/w16.jpeg"
+  },
+  {
+    "title": "PARTH WITH A SOLDERING IRON",
+    "cat": "TESTING",
+    "img": "/gallery img/26 22.jpeg"
+  },
+  {
+    "title": "THANK YOU FOR YOUR DONATION",
+    "cat": "EVENT",
+    "img": "/gallery img/a1.jpg"
+  },
+  {
+    "title": "VERY BADLY CROPPED",
+    "cat": "EVENT",
+    "img": "/gallery img/rc plaane ath.jpeg"
+  },
+  {
+    "title": "CHECKING THE CHARGING",
+    "cat": "PROJECT",
+    "img": "/gallery img/26 2.jpeg"
+  },
+  {
+    "title": "NOW CALCULATING THRUST BY TOROIDAL",
+    "cat": "PROJECT",
+    "img": "/gallery img/q.jpeg"
+  },
+  {
+    "title": "CAN YOU GUESS WHAT THEY ARE TALKING",
+    "cat": "WORKSHOP",
+    "img": "/gallery img/w13.jpeg"
+  },
+  {
+    "title": "GOODBYES TO THE FOUNDING BATCH",
+    "cat": "EVENT",
+    "img": "/gallery img/w15.jpeg"
+  },
+  {
+    "title": "THE IV NIVRUTTI AND HRK(HRISIKESH) IN CLUB COLOURS",
+    "cat": "TESTING",
+    "img": "/gallery img/26 21.jpeg"
+  },
+  {
+    "title": "CHECKING THE CHARGING ON THE STAND",
+    "cat": "PROJECT",
+    "img": "/gallery img/26 3.jpeg"
+  },
+  {
+    "title": "LOOKS DOPE!",
+    "cat": "PROJECT",
+    "img": "/gallery img/a3025.jpg"
+  },
+  {
+    "title": "THE MESS BEHIND THE GLORY",
+    "cat": "WORKSHOP",
+    "img": "/gallery img/w2.jpeg"
+  },
+  {
+    "title": "TEAM OF 2024 @ALUMNI MEET 25",
+    "cat": "EVENT",
+    "img": "/gallery img/expo.jpg"
+  },
+  {
+    "title": "THE TEAM AT ALUMNI MEET 25",
+    "cat": "EVENT",
+    "img": "/gallery img/a2.jpg"
+  },
+  {
+    "title": "CORNERED AT ALUMNI MEET 26 BUT WE ARE CENTER OF ATTRACTION",
+    "cat": "EVENT",
+    "img": "/gallery img/26 1.jpeg"
+  },
+  {
+    "title": "ANOTHER ANGLE OF BLACK WHALE",
+    "cat": "PROJECT",
+    "img": "/gallery img/rc merge.jpg"
+  },
+  {
+    "title": "BLACK WHALE READY TO FLY",
+    "cat": "PROJECT",
+    "img": "/gallery img/w17.jpeg"
+  },
+  {
+    "title": "THE BUILDING OF BLACK WHALE",
+    "cat": "WORKSHOP",
+    "img": "/gallery img/w14.jpeg"
+  },
+  {
+    "title": "ANOTHER WIN TO CONTINUE LEGACY!",
+    "cat": "COMPETITION",
+    "img": "/gallery img/26 12.jpeg"
+  },
+  {
+    "title": "ADITYA AND VAIBHAV (VAIBHAV HAS NOT EATEN SO EATING THE WIRE?)",
+    "cat": "TESTING",
+    "img": "/gallery img/26 23.jpeg"
+  },
+  {
+    "title": "THE VISIT THAT CHANGED SEVERAL LIVES",
+    "cat": "EVENT",
+    "img": "/gallery img/visit.jpg"
+  },
+  {
+    "title": "VTOL UDD!",
+    "cat": "PROJECT",
+    "img": "/gallery img/26 8.jpeg"
+  },
+  {
+    "title": "OUR VISIT TO DRONE EXPO 25 MUMBAI",
+    "cat": "EVENT",
+    "img": "/gallery img/26 14.jpeg"
+  },
+  {
+    "title": "OUR TEAM AT AIR STRIP",
+    "cat": "EVENT",
+    "img": "/gallery img/a4.jpg"
+  },
+  {
+    "title": "LETS PLAY WITH LASER",
+    "cat": "PROJECT",
+    "img": "/gallery img/w26.jpeg"
+  },
+  {
+    "title": "THE PROJECTS TILL 2025",
+    "cat": "PROJECT",
+    "img": "/gallery img/a2025.jpg"
+  },
+  {
+    "title": "STILL CATCHING UP!",
+    "cat": "EVENT",
+    "img": "/gallery img/expo3.jpg"
+  },
+  {
+    "title": "WANNA LEARN PCBs",
+    "cat": "WORKSHOP",
+    "img": "/gallery img/workhop1.jpg"
+  },
+  {
+    "title": "LEGACY KO KISI KI NAZAR NA LAGE",
+    "cat": "COMPETITION",
+    "img": "/gallery img/trophy.jpg"
+  },
+  {
+    "title": "FEW MORE PICS",
+    "cat": "EVENT",
+    "img": "/gallery img/w24.jpeg"
+  },
+  {
+    "title": "THERE ARE 3 CAPTAINS IN ONE PIC",
+    "cat": "EVENT",
+    "img": "/gallery img/w6.jpeg"
+  },
+  {
+    "title": "CALCULATIONS FOR TOROIDAL DESIGN",
+    "cat": "PROJECT",
+    "img": "/gallery img/p.jpeg"
+  }
 ];
